38486e6aad5bf03d4df0f1bba14df144e24d22ef
Jefferson Luiz Cruz <jefferson.luiz.cruz@gmail.com>
2020-10-04 19:14:40 -0300

    Melhorando Exception 1.1.0

--- src/main/java/br/com/estudos/drogaria/exception/CategoriaException.java
+++ src/main/java/br/com/estudos/drogaria/exception/CategoriaException.java
@@ -2,7 +2,7 @@ package br.com.estudos.drogaria.exception;
 
 public class CategoriaException extends RuntimeException{
 
-	private static final long serialVersionUID = 5426706917813669844L;//Esse tratamento de erro não está funcionando. 
+	private static final long serialVersionUID = 5426706917813669844L;
 
 
 }
